65913b0aa75d9a4c3dcee82c87d3776c961f703f
yuriesan <harayuri0407@gmail.com>
2019-07-07 10:17:07 +0900

    test

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
   def index
   end
 
@@ -14,14 +14,18 @@ class GroupsController < ApplicationController
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
+    end
   end
 
   def edit
   end
 
   def update
-  end
-
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
 end
 
 private
