877ba3529c5adb3b67ae08447c15798d2e710bab
yuriesan <harayuri0407@gmail.com>
2019-07-18 19:13:53 +0900

    test

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -24,30 +24,30 @@ $(function(){
     return html;
 }
 
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var message = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: message,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#new_message')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-      $('.submit-btn').attr('disabled', false);
-    })
-    .fail(function(){
-      alert('メッセージを送信できませんでした');
-      $('.submit-btn').attr('disabled', false);
-    })
-  });
+  // $('#new_message').on('submit', function(e){
+  //   e.preventDefault();
+  //   var message = new FormData(this);
+  //   var url = $(this).attr('action')
+  //   $.ajax({
+  //     url: url,
+  //     type: "POST",
+  //     data: message,
+  //     dataType: 'json',
+  //     processData: false,
+  //     contentType: false
+  //   })
+  //   .done(function(data){
+  //     var html = buildHTML(data);
+  //     $('.messages').append(html);
+  //     $('#new_message')[0].reset();
+  //     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+  //     $('.submit-btn').attr('disabled', false);
+  //   })
+  //   .fail(function(){
+  //     alert('メッセージを送信できませんでした');
+  //     $('.submit-btn').attr('disabled', false);
+  //   })
+  // });
 
 
   var reloadMessages = function() {
